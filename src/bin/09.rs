0fffdd6a5276a94ba1effddd5b2c6490dac0c2f4
Marius Niveri <marius.niveri@studium.uni-hamburg.de>
2024-12-10 19:40:41 +0100

    Door 9: Add caching of index to part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -49,134 +47,133 @@ fn parse_input(input: &str) -> Vec<usize> {
     defragged
 }
 
-type File = (usize, u8);
+#[derive(Clone)]
+struct File {
+    id: usize,
+    size: u8,
+}
 
-#[derive(Debug)]
 enum FsEntity {
     File(File),
     FreeSpace(u8),
-    Corruption(u8), // Because according to AOC rules, we cannot us
+    Corruption(u8), // Because according to AOC rules, we cannot use
                     // space freed by a moved file for new files :/
 }
 
 struct Filesystem {
-    content: VecDeque<FsEntity>,
-    _index: usize,
-    _last_found_indices: [usize; 9],
+    file_queue: Vec<FsEntity>,
+
+    /// For each size (1 - 9), cache the last spot
+    /// where we found a file of that size or smaller
+    index_cache: [usize; 10],
+
+    /// The Index needed for calculating the AOC "Checksum"
+    aoc_index: usize,
 }
 
 impl Iterator for Filesystem {
-    type Item = (usize, FsEntity);
+    type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut return_entity: Option<(usize, FsEntity)> = None;
-        if let Some(item) = self.content.pop_front() {
-            match item {
-                FsEntity::File(file) => {
-                    return_entity = Some((self._index, FsEntity::File(file)));
-                    self._index += file.1 as usize;
-                }
-                FsEntity::FreeSpace(free_size) => {
-                    // 1. Get first fitting file from back and replace it with Corruption
-                    //
-                    // 2. Insert file at current spot
-                    //
-                    // 3. Calculate Free Space left
-                    //
-                    // 4. If some left, push_front(free_space_left)
+        match self.file_queue.pop() {
+            Some(FsEntity::FreeSpace(free_size)) => {
+                // 1. Get first fitting file from File Queue and replace it with Corruption
+                // 1.1 Start search at index saved in cache and save index of found item
+                //
+                // 2. Insert file at current spot
+                //
+                // 3. Calculate Free Space left
+                //
+                // 4. If some left, push_front(free_space_left)
 
-                    let first_fitting_file: Option<File> = if let Some(index) =
-                        self.content.iter().rposition(|fse| match fse {
-                            FsEntity::File((_, file_size)) => free_size >= *file_size,
-                            _ => false,
-                        }) {
-                        match self.content.get_mut(index) {
-                            Some(file) => match file {
-                                FsEntity::File(x) => {
-                                    let tmp = Some(*x);
-                                    *file = FsEntity::Corruption(x.1);
-                                    tmp
-                                }
-                                _ => None,
-                            },
-                            _ => None,
-                        }
-                    } else {
-                        None
-                    };
+                match self
+                    .file_queue
+                    .iter_mut()
+                    .skip(self.index_cache[free_size as usize])
+                    .enumerate()
+                    .find(|(_, file_system_entry)| match file_system_entry {
+                        FsEntity::File(file) => free_size >= file.size,
+                        _ => false,
+                    }) {
+                    Some((i, entry_of_first_fitting_file)) => match entry_of_first_fitting_file {
+                        FsEntity::File(file) => {
+                            self.index_cache[free_size as usize] += i + 1;
 
-                    match first_fitting_file {
-                        Some(file) => {
-                            return_entity = Some((self._index, FsEntity::File(file)));
-                            self._index += file.1 as usize;
+                            let i = self.aoc_index;
+                            let s = file.size as usize;
+                            let id = file.id;
+                            self.aoc_index += file.size as usize;
 
-                            let free_space_remaining = free_size - file.1;
+                            let free_space_remaining = free_size - file.size;
+
+                            *entry_of_first_fitting_file = FsEntity::Corruption(file.size);
+
                             if free_space_remaining > 0 {
-                                self.content
-                                    .push_front(FsEntity::FreeSpace(free_space_remaining));
+                                self.file_queue
+                                    .push(FsEntity::FreeSpace(free_space_remaining));
                             }
+
+                            // Math Background:
+                            //      for k in 0..(size - 1) { (i+k) * id }
+                            // Reduces to:
+                            //      (id * size * (2i + s - 1)) / 2
+
+                            Some((id * s * (2 * i + s - 1)) / 2)
                         }
-                        None => {
-                            return_entity = Some((self._index, FsEntity::FreeSpace(free_size)));
-                            self._index += free_size as usize;
-                        }
+                        _ => unreachable!(),
+                    },
+                    _ => {
+                        self.aoc_index += free_size as usize;
+                        Some(0)
                     }
                 }
-                FsEntity::Corruption(size) => {
-                    return_entity = Some((self._index, FsEntity::FreeSpace(size)));
-                    self._index += size as usize;
-                }
             }
+            Some(FsEntity::File(file)) => {
+                let i = self.aoc_index;
+                let s = file.size as usize;
+                self.aoc_index += file.size as usize;
+                Some((file.id * s * (2 * i + s - 1)) / 2) // See Math Background above
+            }
+            Some(FsEntity::Corruption(size)) => {
+                self.aoc_index += size as usize;
+                Some(0)
+            }
+            None => None,
         }
-        return_entity
     }
 }
 
 impl From<&str> for Filesystem {
     fn from(input: &str) -> Self {
-        let mut content: VecDeque<FsEntity> = VecDeque::new();
+        let mut file_queue: Vec<FsEntity> = Vec::new();
 
         let mut is_file = true;
         let mut id = 0;
         for byte in input.trim_end().bytes() {
             if is_file {
-                content.push_back(FsEntity::File((id, byte - 0x30)));
+                file_queue.push(FsEntity::File(File {
+                    id,
+                    size: byte - 0x30,
+                }));
                 id += 1;
             } else if byte > 0x30 {
-                content.push_back(FsEntity::FreeSpace(byte - 0x30));
+                file_queue.push(FsEntity::FreeSpace(byte - 0x30));
             }
             is_file = !is_file;
         }
 
+        file_queue.reverse();
+
         Filesystem {
-            content,
-            _index: 0,
-            _last_found_indices: [
-                id * 2,
-                id * 2,
-                id * 2,
-                id * 2,
-                id * 2,
-                id * 2,
-                id * 2,
-                id * 2,
-                id * 2,
-            ],
+            file_queue,
+            aoc_index: 0,
+            index_cache: [usize::MAX, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         }
     }
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    Some(
-        Filesystem::from(input)
-            .map(|(index, fs_entity)| match fs_entity {
-                FsEntity::File(file) => {
-                    (index..(index + file.1 as usize)).map(|i| i * file.0).sum()
-                }
-                _ => 0,
-            })
-            .sum(),
-    )
+    Some(Filesystem::from(input).sum())
 }
 
 #[cfg(test)]

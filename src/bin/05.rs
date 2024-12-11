af34cd85efc3837aacc2ffbce4f167b7703d5d92
Marius Niveri <marius.niveri@studium.uni-hamburg.de>
2024-12-11 18:27:21 +0100

    Door 5: Solve using map of orderings instead of HashMap

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,39 +1,6 @@
-use std::collections::HashMap;
+use std::cmp::Ordering::*;
 
 advent_of_code::solution!(5);
-
-fn is_ordering_correct(page_ordering: &[usize], rules: &HashMap<usize, Vec<usize>>) -> bool {
-    for (i, page) in page_ordering.iter().enumerate() {
-        for next in page_ordering.iter().skip(i + 1) {
-            if let Some(rule) = rules.get(page) {
-                if rule.contains(next) {
-                    return false;
-                }
-            }
-        }
-    }
-
-    true
-}
-
-// TODO: Very brute-forced method. Do better. https://en.wikipedia.org/wiki/Adjacency_list ?
-fn fix_ordering(page_ordering: &[usize], rules: &HashMap<usize, Vec<usize>>) -> Vec<usize> {
-    let mut fixed_ordering: Vec<usize> = page_ordering.to_owned();
-
-    while !is_ordering_correct(&fixed_ordering, rules) {
-        for (i, page) in fixed_ordering.clone().iter().enumerate() {
-            for j in (i + 1)..(fixed_ordering.len()) {
-                if let Some(rule) = rules.get(page) {
-                    if rule.contains(&fixed_ordering[j]) {
-                        fixed_ordering.swap(i, j);
-                    }
-                }
-            }
-        }
-    }
-
-    fixed_ordering
-}
 
 pub fn part_one(input: &str) -> Option<usize> {
     let (rules, page_orderings) = parse_input(input);
@@ -41,49 +8,49 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(
         page_orderings
             .iter()
-            .filter(|ordering| is_ordering_correct(ordering, &rules))
+            .filter(|ordering| ordering.is_sorted_by(|&left, &right| rules[left][right] == Less))
             .map(|ordering| ordering[ordering.len() / 2])
             .sum(),
     )
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let (rules, page_orderings) = parse_input(input);
+    let (rules, mut page_orderings) = parse_input(input);
 
     Some(
         page_orderings
-            .iter()
-            .filter(|ordering| !is_ordering_correct(ordering, &rules))
-            .map(|ordering| fix_ordering(ordering, &rules))
-            .map(|orderring| orderring[orderring.len() / 2])
+            .iter_mut()
+            .filter(|ordering| !ordering.is_sorted_by(|&left, &right| rules[left][right] == Less))
+            .map(|new_ordering| {
+                let middle = new_ordering.len() / 2;
+
+                // this method orders until the middle and returns (left, middle, right)
+                *new_ordering
+                    .select_nth_unstable_by(middle, |&left, &right| rules[left][right])
+                    .1
+            })
             .sum(),
     )
 }
 
-fn parse_input(input: &str) -> (HashMap<usize, Vec<usize>>, Vec<Vec<usize>>) {
-    let mut rules = HashMap::<usize, Vec<usize>>::new();
+fn parse_input(input: &str) -> ([[std::cmp::Ordering; 100]; 100], Vec<Vec<usize>>) {
+    let mut rules = [[Greater; 100]; 100];
 
     let (rules_str, page_orderings_str) = input.split_once("\n\n").unwrap();
 
-    rules_str.lines().for_each(|line| {
-        let (left, right) = line.split_once("|").unwrap();
-        let right_parsed = &right.parse::<usize>().unwrap();
-        match rules.get_mut(right_parsed) {
-            Some(v) => v.push(left.parse::<usize>().unwrap()),
-            None => {
-                rules.insert(*right_parsed, vec![left.parse::<usize>().unwrap()]);
-            }
-        };
-    });
+    for line in rules_str.lines() {
+        let (left, right): (usize, usize) = line
+            .split_once("|")
+            .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
+            .unwrap();
 
-    let page_orderings = page_orderings_str
+        rules[left][right] = Less;
+    }
+
+    let page_orderings: Vec<Vec<usize>> = page_orderings_str
         .lines()
-        .map(|line| {
-            line.split(",")
-                .map(|s| s.parse::<usize>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+        .map(|line| line.split(",").filter_map(|s| s.parse().ok()).collect())
+        .collect();
 
     (rules, page_orderings)
 }

5b6af112b2e11da04a92aaf34211e320cc3b85f3
Marius Niveri <marius.niveri@studium.uni-hamburg.de>
2024-12-14 17:04:46 +0100

    Door 14: Solve part 2 using Chinese Remainder Theorem

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,30 +1,196 @@
+use std::cmp::Ordering::*;
+
 advent_of_code::solution!(14);
 
-const WIDTH: isize = 101;
-const HEIGHT: isize = 103;
+const WIDTH: usize = 101;
+const HEIGHT: usize = 103;
 
-const Q_WIDTH: isize = WIDTH / 2;
-const Q_HEIGHT: isize = HEIGHT / 2;
+const Q_WIDTH: usize = WIDTH / 2;
+const Q_HEIGHT: usize = HEIGHT / 2;
 
 struct ToiletGuard {
-    x: isize,
-    y: isize,
+    /// current position on x-axis
+    x: usize,
+    /// current position on y-axis
+    y: usize,
+    /// velocity (x, y)
     v: (isize, isize),
 }
 
 impl ToiletGuard {
+    /// Wraps a position value to ensure it stays within the bounds [0, limit).
     #[inline(always)]
-    fn tick(&mut self, ticks: isize) {
-        self.x = (self.x + self.v.0 * ticks) % WIDTH;
-        self.y = (self.y + self.v.1 * ticks) % HEIGHT;
+    fn _wrap(value: isize, limit: usize) -> usize {
+        ((value % limit as isize + limit as isize) % limit as isize) as usize
+    }
 
-        if self.x < 0 {
-            self.x = WIDTH + self.x;
-        }
+    #[inline(always)]
+    fn tick(&mut self, ticks: usize) {
+        self.x = Self::_wrap(self.x as isize + self.v.0 * ticks as isize, WIDTH);
+        self.y = Self::_wrap(self.y as isize + self.v.1 * ticks as isize, HEIGHT);
+    }
 
-        if self.y < 0 {
-            self.y = HEIGHT + self.y;
+    #[inline(always)]
+    fn tick_x_once(&mut self) {
+        self.x = Self::_wrap(self.x as isize + self.v.0, WIDTH);
+    }
+
+    #[inline(always)]
+    fn tick_y_once(&mut self) {
+        self.y = Self::_wrap(self.y as isize + self.v.1, HEIGHT);
+    }
+}
+
+/// Stores the amount of guards in each quadrant.
+struct Quadrants([usize; 4]);
+
+impl FromIterator<(usize, usize)> for Quadrants {
+    fn from_iter<T: IntoIterator<Item = (usize, usize)>>(iter: T) -> Self {
+        let mut quadrants: Quadrants = Quadrants([0; 4]);
+        for (x, y) in iter {
+            match x.cmp(&Q_WIDTH) {
+                Greater => match y.cmp(&Q_HEIGHT) {
+                    Greater => {
+                        quadrants.0[0] += 1;
+                    }
+                    Less => {
+                        quadrants.0[1] += 1;
+                    }
+                    Equal => (),
+                },
+                Less => match y.cmp(&Q_HEIGHT) {
+                    Greater => {
+                        quadrants.0[2] += 1;
+                    }
+                    Less => {
+                        quadrants.0[3] += 1;
+                    }
+                    Equal => (),
+                },
+                Equal => (),
+            }
         }
+        quadrants
+    }
+}
+
+/// Just simulate every guard once by taking the velocity vector times 100
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(
+        input
+            .lines()
+            .map(str::bytes)
+            .map(ToiletGuard::from)
+            .map(|mut e| {
+                e.tick(100);
+                (e.x, e.y)
+            })
+            .collect::<Quadrants>()
+            .0
+            .iter()
+            .product(),
+    )
+}
+
+/// Solve using Chinese Remainder Theorem
+/// (See https://www.reddit.com/r/adventofcode/comments/1he0asr/2024_day_14_part_2_why_have_fun_with_image/)
+///
+/// Basically, we simulate the x coordinates WIDTH times and y coordinates HEIGHT times.
+/// For each step, we calculate the average distance from the center. We also keep track
+/// of the tick at which the average distance has been the smallest.
+///
+/// After completing both loops, we know know, at which tick the center-distance was
+/// at a minimum and we can use the CRT to calculate the position in time, at which both
+/// the x-distance and y-distance was the smallest on average.
+///
+/// That spot in time is the spot in time where we find our Christmas Tree.
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut current_min_x = usize::MAX;
+    let mut current_min_y = usize::MAX;
+    let mut min_tick_x = 0;
+    let mut min_tick_y = 0;
+
+    let mut guards: Vec<ToiletGuard> = input
+        .lines()
+        .map(str::bytes)
+        .map(ToiletGuard::from)
+        .collect();
+
+    for tick in 0..WIDTH {
+        let mut avg_d_from_ctr = 0;
+
+        for guard in guards.iter_mut() {
+            guard.tick_x_once();
+            avg_d_from_ctr += (guard.x as isize - Q_WIDTH as isize).pow(2) as usize;
+        }
+        if avg_d_from_ctr < current_min_x {
+            current_min_x = avg_d_from_ctr;
+            min_tick_x = tick + 1;
+        }
+    }
+
+    for tick in 0..HEIGHT {
+        let mut avg_d_from_ctr = 0;
+
+        for guard in guards.iter_mut() {
+            guard.tick_y_once();
+            avg_d_from_ctr += (guard.y as isize - Q_HEIGHT as isize).pow(2) as usize;
+        }
+        if avg_d_from_ctr < current_min_y {
+            current_min_y = avg_d_from_ctr;
+            min_tick_y = tick + 1;
+        }
+    }
+
+    let result = min_tick_x
+        + ((modular_inverse(WIDTH, HEIGHT) * (min_tick_y - min_tick_x)) % HEIGHT) * WIDTH;
+
+    print_tree(input, result);
+
+    Some(result)
+}
+
+fn modular_inverse(a: usize, m: usize) -> usize {
+    // Extended Euclidean Algorithm to find modular inverse
+    let mut m0 = m;
+    let mut x0 = 0;
+    let mut x1 = 1;
+    let mut a = a;
+
+    while a > 1 {
+        let q = a / m0;
+        let t = m0;
+
+        m0 = a % m0;
+        a = t;
+
+        let t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+
+    x1
+}
+
+fn print_tree(input: &str, solution: usize) {
+    let mut area_outside_bathroom: [[bool; WIDTH]; HEIGHT] = [[false; WIDTH]; HEIGHT];
+
+    input
+        .lines()
+        .map(str::bytes)
+        .map(ToiletGuard::from)
+        .for_each(|mut e| {
+            e.tick(solution);
+            area_outside_bathroom[e.y][e.x] = true;
+        });
+
+    println!();
+
+    for row in area_outside_bathroom {
+        for piece in row {
+            print!("{}", if piece { "#" } else { " " });
+        }
+        println!();
     }
 }
 
@@ -79,7 +245,7 @@ impl From<std::str::Bytes<'_>> for ToiletGuard {
             if char == b',' {
                 break;
             }
-            y += isize::from(char - b'0') * 10_isize.pow(oom);
+            y += usize::from(char - b'0') * 10_usize.pow(oom);
             oom += 1;
         }
 
@@ -90,115 +256,12 @@ impl From<std::str::Bytes<'_>> for ToiletGuard {
             if char == b'=' {
                 break;
             }
-            x += isize::from(char - b'0') * 10_isize.pow(oom);
+            x += usize::from(char - b'0') * 10_usize.pow(oom);
             oom += 1;
         }
 
         Self { x, y, v }
     }
-}
-
-struct Quadrants([usize; 4]);
-
-impl FromIterator<(isize, isize)> for Quadrants {
-    fn from_iter<T: IntoIterator<Item = (isize, isize)>>(iter: T) -> Self {
-        let mut quadrants: Quadrants = Quadrants([0; 4]);
-        for (x, y) in iter {
-            if x > Q_WIDTH {
-                if y > Q_HEIGHT {
-                    quadrants.0[0] += 1;
-                } else if y < Q_HEIGHT {
-                    quadrants.0[1] += 1;
-                }
-            } else if x < Q_WIDTH {
-                if y > Q_HEIGHT {
-                    quadrants.0[2] += 1;
-                } else if y < Q_HEIGHT {
-                    quadrants.0[3] += 1;
-                }
-            }
-        }
-        return quadrants;
-    }
-}
-
-pub fn part_one(input: &str) -> Option<usize> {
-    Some(
-        input
-            .lines()
-            .map(str::bytes)
-            .map(ToiletGuard::from)
-            .map(|mut e| {
-                e.tick(100);
-                (e.x, e.y)
-            })
-            .collect::<Quadrants>()
-            .0
-            .iter()
-            .product(),
-    )
-}
-
-fn print_tree(input: &str, solution: usize) {
-    let mut area_outside_bathroom: [[bool; WIDTH as usize]; HEIGHT as usize] =
-        [[false; WIDTH as usize]; HEIGHT as usize];
-
-    input
-        .lines()
-        .map(str::bytes)
-        .map(ToiletGuard::from)
-        .for_each(|mut e| {
-            e.tick(solution as isize);
-            area_outside_bathroom[e.y as usize][e.x as usize] = true;
-        });
-
-    println!();
-
-    for row in area_outside_bathroom {
-        for piece in row {
-            print!("{}", if piece { "#" } else { " " });
-        }
-        println!();
-    }
-}
-
-pub fn part_two(input: &str) -> Option<usize> {
-    let mut guards: Vec<ToiletGuard> = input
-        .lines()
-        .map(str::bytes)
-        .map(ToiletGuard::from)
-        .collect();
-
-    let mut seconds_ellapsed = 0;
-
-    'main_loop: loop {
-        seconds_ellapsed += 1;
-
-        let mut area_outside_bathroom: [[bool; WIDTH as usize]; HEIGHT as usize] =
-            [[false; WIDTH as usize]; HEIGHT as usize];
-
-        guards.iter_mut().for_each(|guard| {
-            guard.tick(1);
-            area_outside_bathroom[guard.y as usize][guard.x as usize] = true;
-        });
-
-        // check if the area contains a 3x3 spot that is completely filled.
-        // If it does, we assume it to be a christmas tree.
-        for y in 0..(HEIGHT - 2) {
-            'row_loop: for x in 0..(WIDTH - 2) {
-                for i in 0..3 {
-                    for j in 0..3 {
-                        if !area_outside_bathroom[y as usize + i][x as usize + j] {
-                            continue 'row_loop;
-                        }
-                    }
-                }
-                break 'main_loop;
-            }
-        }
-    }
-
-    Some(seconds_ellapsed)
 }
 
 #[cfg(test)]
